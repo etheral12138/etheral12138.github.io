3519e04b1262518faedb823402176b755bcf8458
etheral12138 <1694815257@qq.com>
2023-01-04 04:49:03 +0800

    Site updated: 2023-01-04 04:49:02

---
+++ js/title.js
@@ -0,0 +1,18 @@
+
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '👀一定会有你喜欢的~';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '🐖你终于来啦～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
